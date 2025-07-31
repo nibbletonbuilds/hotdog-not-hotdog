93d1a37a87f7801b053f25600b561db1458586de
nibbletonbuilds <nibbletondhamster@gmail.com>
2025-07-31 22:26:26 +0530

    update test.js

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 const is_hotdog = require('./index');
 
-console.log (is_hotdog ("hot-dog"));
+const test_string_1 = "hot-dog";
+console.log (test_string_1 + " = " + is_hotdog (test_string_1));
 
-console.log (is_hotdog ("pizza"));
-console.log (is_hotdog ("hot-doggie"));
+const test_string_2 = "pizza";
+console.log (test_string_2 + " = " + is_hotdog (test_string_2));
+
+const test_string_3 = "hot doggie";
+console.log (test_string_3 + " = " + is_hotdog (test_string_3));
