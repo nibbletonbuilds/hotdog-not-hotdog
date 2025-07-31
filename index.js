b595d1da2dbe9b0bbacb218e34f79965f60f0896
nibbletonbuilds <nibbletondhamster@gmail.com>
2025-07-31 22:25:58 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function is_hotdog (food_name) {
-    return food_name === "hotdog" ? "hotdog" : "not hotdog";
+    return food_name === "hot-dog" ? "hot-dog" : "not hot-dog";
 }
 
 module.exports = is_hotdog;
